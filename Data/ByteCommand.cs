fd7e988fb764a546002ac493e428da7a9ae5ae3f
Peter Yuan <Peter.Yuan@Tm-robot.com>
2023-03-10 11:30:59 +0800

    James up

---
+++ Data/ByteCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace AGVServer.Data
+{
+	public class ByteCommand
+	{
+		public byte _byte { get; set; }
+		public string _byte_string { get; set; }
+		public string command { get; set; }
+		public ByteCommand(byte _byte, string command)
+		{
+			this._byte = _byte;
+			this._byte_string = BitConverter.ToString(new List<byte>() { this._byte }.ToArray());
+			this.command = command;
+		}
+	}
+}
---
+++ Data/ByteComponent.cs
@@ -0,0 +1,32 @@
+﻿namespace AGVServer.Data
+{
+	public class ByteComponent
+	{
+		public byte nameCode { get; set; }
+		public string nameCode_string { get; set; }
+		public string name { get; set; }
+		public byte postfix { get; set; }
+
+		public ushort maxVal { get; set; }
+		public ByteComponent(byte nameCode, string name)
+		{
+			this.nameCode = nameCode;
+			this.nameCode_string = BitConverter.ToString(new List<byte>() { this.nameCode }.ToArray());
+			this.name = name;
+			this.postfix = 0x20;
+			switch (nameCode)
+			{
+				case 0x58:
+				case 0x59:
+					maxVal = 377;
+					break;
+				case 0x44:
+					maxVal = 7999;
+					break;
+				default:
+					maxVal = 0;
+					break;
+			}
+		}
+	}
+}
